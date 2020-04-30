d9f060f870da715423ef14c3430ec646a7693655
Tooraj Taraz <toorajtaraz@Toorajs-MacBook-Pro-7.local>
2020-04-30 17:25:17 +0430

    creating needed files

---
+++ assembler.h
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  assembler.h
+ *
+ *    Description: assembling mips code 
+ *
+ *        Version:  1.0
+ *        Created:  04/30/2020 17:19:48
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  TOORAJ TARAZ (TOORAJ_TRZ), TOORAJ.INFO@GMAIL.COM
+ *   Organization:  RC|FUM
+ *
+ * =====================================================================================
+ */
+
---
+++ data_types.h
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  data_types.h
+ *
+ *    Description: data types for sc cpu eml 
+ *
+ *        Version:  1.0
+ *        Created:  04/30/2020 17:18:47
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  TOORAJ TARAZ (TOORAJ_TRZ), TOORAJ.INFO@GMAIL.COM
+ *   Organization:  RC|FUM
+ *
+ * =====================================================================================
+ */
+
