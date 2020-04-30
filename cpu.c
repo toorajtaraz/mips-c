c3e7fb637ff2ddfac2f95fe5521d01595ef315af
Tooraj Taraz <toorajtaraz@Toorajs-MacBook-Pro-7.local>
2020-04-30 18:30:24 +0430

    creating backbone of control - R type

--- cpu.c
+++ cpu.c
@@ -16,7 +16,6 @@
  * =====================================================================================
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "data_types.h"
@@ -26,7 +25,7 @@ uint32_t reg[32];
 byte program_memory[33];
 byte data_memory[33];
 int program_counter = 0;
-void init () 
+void init()
 {
     reg[0] = 0;
     for (int i = 1; i < 32; i++)
@@ -40,85 +39,95 @@ void init ()
         data_memory[i] = -1;
     }
     program_counter = 0;
-    
 }
 
-void get_input ()
+void get_input()
 {
-    scanf("%33s",program_memory);
-    program_memory[32] = '\0';   
+    scanf("%33s", program_memory);
+    program_memory[32] = '\0';
 }
-void alu (int alu_fun, uint8_t address1, uint8_t address2)
+void alu(int alu_fun, uint8_t address1, uint8_t address2)
 {
-
 }
 
-void alui (int alu_fun, uint8_t address1, uint32_t immidiate)
+void alui(int alu_fun, uint8_t address1, uint32_t immidiate)
 {
-
-} 
-void control_unit ()
+}
+void control_unit()
 {
     //R type
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '0' && program_memory[3] == '0' && program_memory[4] == '0' && program_memory[5] == '0')
     {
-        
+        //ADD
+        if (program_memory[26] == '1' && program_memory[27] == '0' && program_memory[28] == '0' && program_memory[29] == '0' && program_memory[30] == '0' && program_memory[31] == '0')
+        {
+            /* code */
+        }
+        //SUB
+        if (program_memory[26] == '1' && program_memory[27] == '0' && program_memory[28] == '0' && program_memory[29] == '0' && program_memory[30] == '1' && program_memory[31] == '0')
+        {
+            /* code */
+        }
+        //OR
+        if (program_memory[26] == '1' && program_memory[27] == '0' && program_memory[28] == '0' && program_memory[29] == '1' && program_memory[30] == '1' && program_memory[31] == '0')
+        {
+            /* code */
+        }
+        //AND
+        if (program_memory[26] == '1' && program_memory[27] == '0' && program_memory[28] == '0' && program_memory[29] == '1' && program_memory[30] == '0' && program_memory[31] == '0')
+        {
+            /* code */
+        }
+        //SLT
+        if (program_memory[26] == '1' && program_memory[27] == '0' && program_memory[28] == '1' && program_memory[29] == '0' && program_memory[30] == '1' && program_memory[31] == '0')
+        {
+            /* code */
+        }
     }
     //I type
     //SW
     if (program_memory[0] == '1' && program_memory[1] == '0' && program_memory[2] == '1' && program_memory[3] == '0' && program_memory[4] == '1' && program_memory[5] == '1')
     {
-        
     }
     //LW
     if (program_memory[0] == '1' && program_memory[1] == '0' && program_memory[2] == '0' && program_memory[3] == '0' && program_memory[4] == '1' && program_memory[5] == '1')
     {
-        
     }
     //ADDI
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '1' && program_memory[3] == '0' && program_memory[4] == '0' && program_memory[5] == '0')
     {
-        
     }
     //SLTI
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '1' && program_memory[3] == '0' && program_memory[4] == '1' && program_memory[5] == '0')
     {
-        
     }
     //ANDI
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '1' && program_memory[3] == '1' && program_memory[4] == '0' && program_memory[5] == '0')
     {
-        
     }
     //ORI
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '1' && program_memory[3] == '1' && program_memory[4] == '0' && program_memory[5] == '1')
     {
-        
     }
     //BEQ
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '0' && program_memory[3] == '1' && program_memory[4] == '0' && program_memory[5] == '0')
     {
-        
     }
     //BNE
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '0' && program_memory[3] == '1' && program_memory[4] == '0' && program_memory[5] == '1')
     {
-        
     }
     //J type
     //J
     if (program_memory[0] == '0' && program_memory[1] == '0' && program_memory[2] == '0' && program_memory[3] == '0' && program_memory[4] == '1' && program_memory[5] == '0')
     {
-        
     }
 }
 
-void sign_extender ()
+void sign_extender()
 {
-
 }
 
-int main ()
+int main()
 {
-    
 }
